a62b88b3309f12d4d5959cb46d6b710982631e17
Patrick Hermann <patrick.hermann@sva.de>
2025-04-15 19:16:51 +0000

    Changed: Taskfile.yaml example/list/main.go go.mod list.go model.go

---
+++ example/list/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea/v2"
+
+	"github.com/stuttgart-things/survey"
+)
+
+var (
+	output      string
+	defaultVars = []string{
+		"manage_filesystem+-true",
+		"update_packages+-true",
+		"install_requirements+-true",
+		"install_motd+-true",
+		"username+-sthings",
+		"lvm_home_sizing+-'15%'",
+		"lvm_root_sizing+-'35%'",
+		"lvm_var_sizing+-'50%'",
+	}
+)
+
+func main() {
+	p := tea.NewProgram(survey.InitListModel(defaultVars))
+	m, err := p.Run()
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+
+	if model, ok := m.(survey.ListModel); ok && model.FinalOutput != "" {
+		fmt.Println("\nSaved variables:")
+		fmt.Print(model.FinalOutput)
+		output = model.FinalOutput
+	}
+
+	fmt.Println("\nFinal output:", output)
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/charmbracelet/lipgloss/v2 v2.0.0-beta1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.10.0
-	gopkg.in/yaml.v3 v3.0.1
+	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
